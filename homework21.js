3380c940f2c3fc1c54603486336b151184adca39
LementaD <132382934+LementaD@users.noreply.github.com>
2023-07-05 20:24:13 +0300

    Add files via upload

---
+++ homework21.js
@@ -0,0 +1,98 @@
+//Arrow to Functions
+
+//****************** Исходник 1 ********************************
+{
+    (temprCel) => { (temprCel * 9 / 5) + 32
+        return temprFar
+    }
+}
+//**************************************************************
+
+{
+    function f (temprCel) { 
+        return (temprCel * 9 / 5) + 32
+    }
+}
+
+//****************** Исходник 2 ***********************************************
+
+{
+    () => {
+        let str = prompt ("Введіть прізвище").trim ()
+        let str2 = prompt ("Введіть ім'я").trim ()
+        let str3 = prompt ("Введіть по батькові").trim ()
+        SurName = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+        Name = str2.charAt(0).toUpperCase() + str2.slice(1).toLowerCase()
+        FatherName = str3.charAt(0).toUpperCase() + str3.slice(1).toLowerCase()
+        FullName = SurName + " " + Name + " " + FatherName   
+    return {Name, SurName, FatherName, FullName}
+    }
+}
+
+//*****************************************************************************
+
+{
+    function f () {
+        let str = prompt ("Введіть прізвище").trim ()
+        let str2 = prompt ("Введіть ім'я").trim ()
+        let str3 = prompt ("Введіть по батькові").trim ()
+        SurName = str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+        Name = str2.charAt(0).toUpperCase() + str2.slice(1).toLowerCase()
+        FatherName = str3.charAt(0).toUpperCase() + str3.slice(1).toLowerCase()
+        FullName = SurName + " " + Name + " " + FatherName   
+    return {Name, SurName, FatherName, FullName}
+    }
+}
+
+//****************** Исходник 3 *************
+
+{
+    const getInputValue = (promptText, defaultValue) => prompt(promptText) || defaultValue;
+    let age = getInputValue("Ваш вік?", "");
+    let year = getInputValue("Який наразі рік?", "");
+    let year_of_birth = year - age || alert("Помилка");
+    alert(year_of_birth);
+    }
+
+//*******************************************
+
+{
+    const getInputValue = function f (promptText, defaultValue) {prompt(promptText)} || defaultValue;
+    let age = getInputValue("Ваш вік?", "");
+    let year = getInputValue("Який наразі рік?", "");
+    let year_of_birth = year - age || alert("Помилка");
+    alert(year_of_birth);
+}
+
+//createPerson_____________________________________________________________________________________________________________________________________________________
+
+{
+    function createPerson(name, surname) {
+        return {
+          name: name,
+          surname: surname,
+          getFullName: function() {
+            return this.name + " " + this.surname;
+          }
+        };
+      }
+      
+      // Приклад використання:
+      const person = createPerson("John", "Doe");
+      console.log(person.name); // John
+      console.log(person.surname); // Doe
+      console.log(person.getFullName()); // John Doe
+}
+
+//createPersonClosure________________________________________________________________________________________________________________________________
+{
+
+}
+//createPersonClosureDestruct________________________________________________________________________________________________________________________
+{
+
+}
+//isSorted____________________________________________________________________________________________________________________________________________
+{
+    
+}
